312521afe1cbd2ca49982e9451206761de70d2e6
Eduardo Pozzi <eduardoampozzi@gmail.com>
2024-04-16 12:28:25 -0300

    Adiciona Atualização de pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosListagemPaciente;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,6 +27,13 @@ public class PacienteController {
             @PageableDefault(size=10, sort = {"nome"}) Pageable paginacao
     ){
         return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualiza(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        Paciente paciente = repository.getReferenceById(dados.id());
+        paciente.atuzalizarInformacoes(dados);
     }
 
 }
---
+++ src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,11 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente(
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco
+) {
+}
