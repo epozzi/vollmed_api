76af719df238c000f0fb2fababea2d07258b3b08
Eduardo Pozzi <eduardoampozzi@gmail.com>
2024-04-16 14:59:37 -0300

    Codigo Curso boas praticas Aula 1

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
@@ -20,32 +20,66 @@ public class MedicoController {
 
     @PostMapping
     @Transactional // Quando atualiza informações no BD precisa da anotação Transational
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(
+            @RequestBody @Valid DadosCadastroMedico dados,
+            UriComponentsBuilder uriBuilder
+    ) {
+        // Resposta padrão para métodos post é 201 e deve conter as informações no corpo da resposta e ainda um cabeçaljo
+        var medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(
+    public ResponseEntity<Page<DadosListagemMedico>> listar(
             @PageableDefault(size=10, sort = {"nome"}) //Define parâmetros default que podem ser sobrescrevidos pela URL
             Pageable paginacao //atenção para não importar o Pageble do java.utils
     ) { //paginação com spring
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new));
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         // Por ter anotado o método como transação a JPA já entende que o put vai atualizar o registro instanciado
         // Então só é preciso instanciar o registro e assim que as atualizações forem feitas nele a JPA já atualiza
         // coisa de maluco isso
         Medico medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         Medico medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build(); // Reposta para delete é 204 - ok noContent
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        Medico medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 }
+
+//Os códigos HTTP (ou HTTPS) possuem três dígitos, sendo que o primeiro dígito significa a classificação dentro das possíveis cinco categorias.
+//
+//1XX: Informativo – a solicitação foi aceita ou o processo continua em andamento;
+//
+//2XX: Confirmação – a ação foi concluída ou entendida;
+//
+//3XX: Redirecionamento – indica que algo mais precisa ser feito ou precisou ser feito para completar a solicitação;
+//
+//4XX: Erro do cliente – indica que a solicitação não pode ser concluída ou contém a sintaxe incorreta;
+//
+//5XX: Erro no servidor – o servidor falhou ao concluir a solicitação.
+
+
+
--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -16,31 +20,76 @@ public class PacienteController {
     @Autowired
     private PacienteRepository repository;
 
+    /*
+     * Para requisições via HTTP ou HTTPs existem padrões de respostas.
+     * No SpringBoot as respostas então padronizadas dentro do ResponseEntity.
+     * Para cadastrar (POST):
+     *   -> Padrão de resposta:
+     *       * Código 201
+     *       * No corpo da resposta devolve os dados cadastrados
+     *       * No header devolve a URL no serviço para detalhar os dados cadastrados
+     *
+     * Para consultar (GET):
+     *   -> Padrão de resposta:
+     *       * Código 200
+     *       * No corpo da resposta devolve os dados da consulta
+     *
+     * Para atualizar (PUT):
+     *   -> Padrão de resposta:
+     *       * Código 200
+     *       * No corpo da resposta devolve os dados atualizados
+     *
+     * Para excluir (DELETE)
+     *  -> Padrão de resposta:
+     *       * Código 204
+     *       * Corpo da resposta sem conteúdo
+     * */
+
     @PostMapping
     @Transactional
-    public void cadastar(@RequestBody @Valid DadosCadastroPaciente dados){
-        repository.save(new Paciente(dados));
+    public ResponseEntity cadastar(
+            @RequestBody @Valid DadosCadastroPaciente dados,
+            UriComponentsBuilder uriBuilder
+    ){
+        Paciente paciente = new Paciente(dados);
+        repository.save(paciente);
+
+        URI uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 
     @GetMapping
-    public Page<DadosListagemPaciente> listar(
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(
             @PageableDefault(size=10, sort = {"nome"}) Pageable paginacao
     ){
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+        return ResponseEntity.ok(
+                repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new)
+        );
     }
 
     @PutMapping
     @Transactional
-    public void atualiza(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+    public ResponseEntity atualiza(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
         Paciente paciente = repository.getReferenceById(dados.id());
         paciente.atuzalizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         Paciente paciente = repository.getReferenceById(id);
         paciente.excluir();
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        Paciente paciente = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
 }
