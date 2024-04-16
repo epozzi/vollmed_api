5fd80fdd2d42b7ab950a343ec1f76a992de69d1f
Eduardo Pozzi <eduardoampozzi@gmail.com>
2024-04-16 11:59:34 -0300

    Adiciona Listagem de pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,14 +1,16 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -18,8 +20,16 @@ public class PacienteController {
     private PacienteRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastar(@RequestBody @Valid DadosCadastroPaciente dados){
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(
+            @PageableDefault(size=10, sort = {"nome"}) Pageable paginacao
+    ){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
 }
